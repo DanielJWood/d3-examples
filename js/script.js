9332df0386f48ac84000b278993b8c755641eed6
Daniel Wood <daniel.j.wood11@gmail.com>
2015-06-15 15:53:43 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,72 @@
-// Insert your scripts here
+var dataset = [];
+var numPoints = 100;
+var numWaves = 4;
+var i, o, p;
+var q = 0;
 
-// Initial Script
+for (i = 0; i < numPoints; i += 1) {
+	p = i / (numPoints - 1) * 100;
+	o = Math.PI * p * numWaves * 2;
+	dataset.push({ percent: p, offset: o });
+}
 
-		var map = L.map('map', {
-			scrollWheelZoom: false
-		}).fitBounds([
-            [42.461, -56.979],[32.536, -134.4]
-            ]);
+var svg = d3.select("#circles")
+	.append("svg")
+	.attr("width", "100%")
+	.attr("height", "700px");
 
-		var cloudmade = L.tileLayer("http://a.tiles.mapbox.com/v3/energy.map-461t6jk2/{z}/{x}/{y}.png", {
-			//attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-			//key: 'BC9A493B41014CAABB98F0471D759707',
-			//styleId: 22677
-		}).addTo(map);
+
+var circles = svg.selectAll("circle")
+	.data(dataset)
+	.enter()
+	.append("circle")
+	.attr("cx", function(d) {
+		return d.percent + "%";
+	})
+	.attr("cy", "50%")
+	.attr("r", "1%");
+
+wave = function(d) {
+if (q === 99) {q=0} else{q += 1;};
+
+// q = q / 100;
+console.log(q)
+// console.log(d.percent)
+  //Move to bottom
+  d3.select(this)
+    .transition()
+    .duration(900)
+    // .attr("cy", "0%")
+    .attr("fill", "hsl(" + (d.percent * 360) + ",100%,50%)")
+    .attr("r", function(d) {
+		return ((Math.sin(d.percent / 5) + 1.1) * 3) + "%";
+	})
+    .each("end", function() {
+
+      //Move to top
+      d3.select(this)
+        .transition()
+        .duration(900)
+        // .attr("cy", "100%")
+        .attr("fill", function(d) {
+        	console.log(q)
+        	var color = "hsl(" + (360-(d.percent * 360)) + ",100%,50%)"
+        	return color;
+        })
+        .attr("r", function(d) {
+			return ((Math.sin(d.percent / 5) + 10.1) * 2) + "%";
+		})
+        .each("end", wave);
+
+    });
+
+};
+
+d3.selectAll("circle")
+  .transition()
+  .delay(function(d, i) {
+    return i * 50;
+  })
+  .duration(900)
+  // .attr("cy", "100%")
+  .each("end", wave)
